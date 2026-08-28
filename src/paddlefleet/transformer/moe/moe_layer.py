5968020c0801b55597f1edc8dbb3bde561f2aadd
genghaozhe <genghaozhe@baidu.com>
2026-08-28 20:21:46 +0800

    code style

--- src/paddlefleet/transformer/moe/moe_layer.py
+++ src/paddlefleet/transformer/moe/moe_layer.py
@@ -42,11 +42,11 @@ if TYPE_CHECKING:
 from paddlefleet import utils
 from paddlefleet.recompute_utils import need_recompute_in_first_n
 from paddlefleet.transformer.activations import situ
-from paddlefleet.transformer.paddle_norm import WrappedPaddleNorm
 from paddlefleet.transformer.dw_overlap import (
     deferrable_linear_bare,
     install_sonic_moe_dw_deferral,
 )
+from paddlefleet.transformer.paddle_norm import WrappedPaddleNorm
 from paddlefleet.transformer.transformer_config import dw_overlap_enabled
 from paddlefleet.transformer.utils import profile
 
@@ -976,7 +976,6 @@ class MoELayer(nn.Layer):
                 hidden_states,
             )
 
-
         return hidden_states
 
     def fusion_moe_forward(
@@ -1077,7 +1076,6 @@ class MoELayer(nn.Layer):
                 self.fc2_latent_proj,
                 hidden_states,
             )
-
 
         return hidden_states
 
